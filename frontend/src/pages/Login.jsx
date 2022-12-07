8e022a835b542a7da7728390fc238ed319573890
Robert Phillips <robert_phillips17@hotmail.co.uk>
2022-12-07 16:36:51 +0000

    login page tweaked

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
     password: ''
   })
 
-  const { name, email, password, password2 } = formData;
+  const { email, password } = formData;
 
   const onChange = (e) => {
     setFormData((prevState) => ({
